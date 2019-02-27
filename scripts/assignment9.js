6ac550a3b3eba34cbb0c2cfbc8da3dad42d8f2e7
Haythem Jlassi <34671817+Haythem55@users.noreply.github.com>
2019-02-27 00:03:06 -0800

    Add files via upload

--- scripts/assignment9.js
+++ scripts/assignment9.js
@@ -18,13 +18,13 @@ c2.init('Baleno');
 function getWeather(){
     $('.weatherResponse').html('');
     let cityName= $('#cityName').val();
-    //I think there is a problem here
+    //I tried different links to the weather api here and couldn't solve this problem. I keep getting an erro
     let apiCall= "http://www.api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=1e6ef7c0afeab7ed8fdac79e0a80b27e";
-    $.getJSON(apiCall, callBack);
+    $.getJSON(apiCall, weatherCallback);
     
-    function callBack(data){
+    /*function wheatherCallback(data){
         console.log(data.weather);
-    }
+    }*/
     function weatherCallback(weatherData){
         let cityName = weatherData.name;
         let country = weatherData.sys.country;
